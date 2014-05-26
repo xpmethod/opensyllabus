97692f1e45703f441d316bd1df95d997b5cb1e24
Mark Gorenstein <markagorenstein@gmail.com>
2014-05-26 14:10:54 -0400

    PDFBox integration for #30

---
+++ extractor_research/extractors/pdfbox.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import os
+
+class PDFBox:
+    def __init__(self, pdf_file, txt_file):
+        self.pdf_file = pdf_file
+        self.txt_file = txt_file
+
+    def extract(self):
+        command = 'java -jar pdfbox-app-1.8.5.jar ExtractText ' + self.pdf_file + ' ' + self.txt_file
+        os.system(command)
+
+if __name__ == '__main__': 
+    pdf = PDFBox('../input/pride_and_prej/1.pdf', '../output/pride_and_prej/pdfbox/1.txt')
+    pdf.extract()
